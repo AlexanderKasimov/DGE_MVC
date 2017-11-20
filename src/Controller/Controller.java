5649782928a78a5e4e18ab4fca314439d9f109eb
AlexanderKasimov <strelokstalk@yandex.ru>
2017-11-21 00:21:01 +0500

    Update

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,21 +2,29 @@ package Controller;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+
 
 public class Controller {
     @FXML
     AnchorPane ap = new AnchorPane();
 
-    public void AddLineClicked(MouseEvent mouseEvent)
-    {
-
+    public void AddLineClicked(MouseEvent mouseEvent)  {
 
         //DoubleProperty
         Line l1 = new Line(Math.random()*500,Math.random()*500,Math.random()*500+100,Math.random()*500+100);
+        DropShadow ds = new DropShadow();
+        ds.setOffsetX(3.0);
+        ds.setOffsetY(3.0);
+        //ds.setSpread(20);
+        ds.setColor(Color.RED);
+        l1.setEffect(ds);
         ap.getChildren().add(l1);
+
 
     }
 }
